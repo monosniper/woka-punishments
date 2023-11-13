3b3bd2cd4324ccb53a1958eef7b2cd099e5c66a6
monosniper <vanteateam@gmail.com>
2023-11-13 22:18:58 +0500

    d

--- get.php
+++ get.php
@@ -1,5 +1,15 @@
 <?php
 
+include_once("db.php");
+
+$db = new DB();
+
+$bans = $db->getBans(0, '');
+$mutes = $db->getMutes(0, '');
+$kicks = $db->getKicks(0, '');
+
 echo json_encode([
-    'shaxzod' => 'lox'
+    'bans' => $bans,
+    'mutes' => $mutes,
+    'kicks' => $kicks,
 ]);
